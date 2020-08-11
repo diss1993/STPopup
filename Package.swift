e70760eb0dfb06dc3b04e5f5131755c67e23200b
dmitry <Buza.dima93@gmail.com>
2020-08-11 13:51:05 +0600

    header fix

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             name: "STPopup",
             path: "STPopup",
             cSettings: [
-                .headerSearchPath("STPopup"),
+                .headerSearchPath("UI"),
             ]
         )
     ]
